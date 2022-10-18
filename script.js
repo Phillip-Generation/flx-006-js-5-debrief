d091a8a5ce8849eb15e949346279aadb939ab261
wisda <crwisda@gmail.com>
2022-10-18 08:42:23 -0700

    added random name generator

---
+++ script.js
@@ -0,0 +1,44 @@
+const names = ['Wil','Ify', 'Charles', 'Kenard', 'Sint', 'Phil', 'Belara', 'Sharesse', 'Kathryn', 'Amare']
+
+//function generateName() {
+   // var listOfNames = document.createElement('ul');
+    //listOfNames.id = 'list-names';
+    //document.getElementById('nameList').appendChild(listOfNames)
+   // for (i=0; i < 5; i++) {
+    //var classNames = names[Math.floor(Math.random() * names.length)];
+   // var listItems = document.createElement('li');
+   // listItems.id = 'lists';
+   // document.getElementById('list-names').appendChild(listItems).innerHTML = classNames
+   // }   
+//}
+
+
+//my-button.onclick = generateName
+//generateName(4)
+
+
+
+function randomNames() {
+    document.getElementById('nameList').innerHTML = " ";
+    var listOfNames = document.createElement('ul');
+    listOfNames.id = 'list-names';
+    document.getElementById('nameList').appendChild(listOfNames);
+    for (let i = 0; i < 5; i++) {
+    var classNames = names[Math.floor(Math.random() * names.length)];
+    var listItems = document.createElement('li');
+    listItems.id = 'lists';
+    document.getElementById('list-names').appendChild(listItems).innerHTML = classNames
+    
+    }
+}
+
+
+
+function clearNames() { 
+    document.getElementById('nameList').innerHTML = " ";
+};
+
+
+myButton.onclick = randomNames;
+clearButton.onclick = clearNames;
+
