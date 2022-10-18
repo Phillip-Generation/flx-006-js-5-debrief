8b71684595fca1d6d6f906c453ad00c35db323ed
javi <12883412+xaviif@users.noreply.github.com>
2022-10-18 12:44:25 -0400

    Delete feature + tweaks

--- assets/javascript/library.js
+++ assets/javascript/library.js
@@ -1,27 +1,30 @@
-const random = (min, max) => min + (Math.random()*max)
+const random = (min, max) => min + Math.random()*(max-min)
 const $names = $("#names")
 const grabRandomName = x => {
   //x = quanity
   let build = [];
-  for(let i = 0; i<x; i++){
-    build[i] = names[Math.floor(random(0, names.length))];
-  }
+  for(let i = 0; i<x; i++) build[i] = names[Math.floor(random(0, names.length))];
   return build;
 }
+const clearHistory = () => {
+  $names.css({"margin-left": 0})
+  $names.html(" ")
+  $("#desc").html("Cleared history.")
+}
+
 const animateNames = x => {
   //https://stackoverflow.com/a/5003287
   displayNames(x)
   let $mainList = $('.mainList');
 
-  $mainList.css('height', 'auto');
+  //$mainList.css('max-height', 'auto');
   let autoHeight = $mainList.height();
-  $mainList.height(0).animate({height: autoHeight}, 450);
+  $mainList.height(0).animate({height: autoHeight}, 650 + ($mainList.children().length*100));
 
-  let mainDivWidth = parseInt($($('#names').children()[1]).css("width"));
+  let mainDivWidth = parseFloat($($('#names').children()[1]).css("width"));
   if($names.children().length > 1) $names.animate({"margin-left": `-=${mainDivWidth}px`}, 1000 )
   
   $('ul:not(.mainList)').animate({opacity: "-=0.175"}, 900)
-    
 }
 const displayNames = x => {
   //x = array of names we wish to display
@@ -33,27 +36,20 @@ const displayNames = x => {
   build+="<ul class='mainList'>";
   for(let i in x) build+=`<li>${x[i]}</li>`
   build+="</ul>"
-  //document.getElementById('names').innerHTML = build
   $names.prepend($.parseHTML(build))
-
 }
 const refreshNames = () =>{
   let numNames = parseInt(window.prompt("How many names?"))
   if(numNames > 20) return window.alert("Please enter any number below 20.") 
-  if(numNames === 0) return 
+  else if(numNames === 0) return 
+  else if(numNames < 0) return window.alert("...")
   let chosenNames = grabRandomName(numNames)
   $(".mainList").removeClass();
   animateNames(chosenNames)
-
 }
 
-const clearHistory = () => {
-  $names.css({"margin-left": 0})
-  $names.html(" ")
-  $("#desc").html("Cleared history.")
-}
-
-const namesToArray = () =>{
-  names = names.trim().split("\n");
-  for(x in names) names[x] = names[x].trim();
+String.prototype.toArray = function(){
+  let s = this.trim().split("\n");
+  for(x in s) s[x] = s[x].trim();
+  return s
 }
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,12 +11,33 @@ let names = `
   Candice N.
   Imagine Dracon
   Dees N.
-`
-//Convert names into an array.
-namesToArray();
+`.toArray();
 
 let button = document.getElementById("generateNamesButton")
 button.onclick = refreshNames;
 
 let clearButton = document.getElementById('clearButton')
 clearButton.onclick = clearHistory
+
+$(document).on("mouseenter", "#names ul:not(:first-child)", function(){
+  $(this).addClass("highlightItem")
+})
+
+$(document).on("mouseleave", "#names ul:not(:first-child)", function(){
+  $(this).removeClass("highlightItem")
+})
+
+$(document).on("click", "#names ul:not(:first-child):not([deleting])", function(){
+  $(this).attr('deleting', '')
+  $(this).off()
+  $('#names ul:not(:first-child):not([deleting])').css("opacity", "+=0.175")
+  let delWidth = parseFloat($(this).css('width'))
+  $('#names').animate({'margin-left': `+=${delWidth}px`}, 1505)
+
+  $(this).children().each(function(){
+    $(this).animate({"width": `0px`}, random(1300,1603))
+  })
+  $(this).animate({'width': 0}, 1500, function(){
+    $(this).remove()
+  });
+})
